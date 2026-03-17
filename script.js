c6da9e0230da9d0319a69683adbdbcbd057c69bd
atakan <78735557+honkard@users.noreply.github.com>
2026-03-17 23:53:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -236,7 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   const bioMessages = [
-    "Fu*k Guns.lol & Fakecrime.bio got banned too often, so I created my own.",
+    //"Fu*k Guns.lol & Fakecrime.bio got banned too often, so I created my own.",
     "\"Hello, World!\""
   ];
   let bioText = '';
